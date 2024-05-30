1b39ace8e58e6b93cb7bd3b35039320a1cdbb2c8
Fredrik Johansson <ml@fredjo.com>
2024-05-30 14:18:11 +0200

    added replication script

---
+++ replicate.sh
@@ -0,0 +1,10 @@
+python fit.py
+python sample.py
+
+python estimate.py
+python evaluate.py
+python present_results.py
+
+python estimate.py -c configs/estimation_minimal.yml
+python evaluate.py -c configs/estimation_minimal.yml
+python present_results.py -c configs/estimation_minimal.yml
